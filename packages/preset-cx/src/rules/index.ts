3a62c5e8ab23e7417c4ad6f0567c60e589ee9aad
linyudeng <linyudeng@limayao.com>
2023-08-21 17:58:12 +0800

    feat: add safeAreaRules

--- packages/preset-cx/src/rules/index.ts
+++ packages/preset-cx/src/rules/index.ts
@@ -1,11 +1,13 @@
 import type { Rule } from 'unocss'
 import type { PresetCXOption } from '../type'
 import { legacyRules } from './legacy-rules'
+import { safeAreaRules } from './safe-area-rules'
 
 export function getRules(option: PresetCXOption) {
   const result: Rule[] = []
   if (option.legacySupport)
     result.push(...legacyRules)
+  result.push(...safeAreaRules)
 
   return result
 }
